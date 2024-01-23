cdb066cc95adf28eeb845def5b1ef97dd08528a2
Seema-Datafortune <seema.sharma@datafortune.com>
2024-01-23 13:20:36 +0530

    Removed swagger.

--- Startup.cs
+++ Startup.cs
@@ -15,7 +15,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
+//using Microsoft.OpenApi.Models;
 using System.Text;
 
 namespace DF_EvolutionAPI
@@ -134,30 +134,30 @@ namespace DF_EvolutionAPI
                 });
             });
 
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "DF_EvolutionAPI", Version = "v1" });
-                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-                {
-                    In = ParameterLocation.Header,
-                    Description = "Please insert JWT with Bearer into field",
-                    Name = "Authorization",
-                    Type = SecuritySchemeType.ApiKey
-                });
-                c.AddSecurityRequirement(new OpenApiSecurityRequirement {
-                   {
-                     new OpenApiSecurityScheme
-                     {
-                       Reference = new OpenApiReference
-                       {
-                         Type = ReferenceType.SecurityScheme,
-                         Id = "Bearer"
-                       }
-                      },
-                      new string[] { }
-                    }
-                  });
-            });
+            //services.AddSwaggerGen(c =>
+            //{
+            //    c.SwaggerDoc("v1", new OpenApiInfo { Title = "DF_EvolutionAPI", Version = "v1" });
+            //    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            //    {
+            //        In = ParameterLocation.Header,
+            //        Description = "Please insert JWT with Bearer into field",
+            //        Name = "Authorization",
+            //        Type = SecuritySchemeType.ApiKey
+            //    });
+            //    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+            //       {
+            //         new OpenApiSecurityScheme
+            //         {
+            //           Reference = new OpenApiReference
+            //           {
+            //             Type = ReferenceType.SecurityScheme,
+            //             Id = "Bearer"
+            //           }
+            //          },
+            //          new string[] { }
+            //        }
+            //      });
+            //});
 
             services.AddControllers();
 
@@ -175,11 +175,6 @@ namespace DF_EvolutionAPI
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c =>
-                {
-                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "DF_EvolutionAPI v1");
-               } ) ;
             }
 
             app.UseHttpsRedirection();
